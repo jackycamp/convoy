16918b0f806a24764953cd68aa8ecd63fa85ad90
jackycamp <jackcampanella@protonmail.com>
2025-01-29 10:03:01 -0500

    rm dns cluster query config

--- config/runtime.exs
+++ config/runtime.exs
@@ -36,7 +36,7 @@ if config_env() == :prod do
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
-  config :convoy, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
+  # config :convoy, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :convoy, ConvoyWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
