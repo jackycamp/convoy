929f1131044e9d67ee806228e9253e9da9e36e2a
jackycamp <jackcampanella@protonmail.com>
2025-01-31 16:39:32 -0500

    adjusting connection timeout

--- config/runtime.exs
+++ config/runtime.exs
@@ -30,7 +30,7 @@ if config_env() in [:prod, :dev] do
       environment variable RAILWAY_API_URL is missing.
       """
 
-  Neuron.Config.set(url: railway_url)
+  Neuron.Config.set(url: railway_url, connection_opts: [recv_timeout: 15_000])
 
   railway_token =
     System.get_env("RAILWAY_TOKEN") ||
